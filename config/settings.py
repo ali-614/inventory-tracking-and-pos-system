863b5b2055d8b076dedaba4c60a388ceee7b7767
ali-614 <alhamdani840@gmail.com>
2026-06-28 05:03:11 +0300

    added the inventory models

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'textile_shop',
+        'USER':'ali',
+        'PASSWORD':'Ali2007@',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Varient(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='varients')
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    reference_number = models.CharField(max_length=50, unique=True)
+    barcode = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.color} - {self.size}"
+    
+
+class StockEntry(models.Model):
+    varient = models.ForeignKey(Varient, on_delete=models.CASCADE, related_name= 'stock_entries')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='stock_entries')
+    quantity = models.PositiveBigIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('varient', 'location')
+
+    def __str__(self):
+        return f"{self.variant} @ {self.location.name}: {self.quantity}"
