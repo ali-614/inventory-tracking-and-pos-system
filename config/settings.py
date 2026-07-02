3afaf4f79cdc7b11d899e2d794351ec39dd3f588
ali-614 <alhamdani840@gmail.com>
2026-07-02 08:38:45 +0300

    added test for barcode generating

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i8!h^1k4x2)ri01joa_!)t$a^yxjm@0l9xe5)!hm%0b_bw%33+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Product, Variant
 # Create your tests here.
+
+class BarcodeGenerationTest(TestCase):
+    def test_barcode_auto_generated(self):
+        product = Product.objects.create(name="Test Zipper", category = "Zippers")
+        variant = Variant.objects.create(
+            product=product,
+            color="Green",
+            size="10cm",
+            reference_number="TEST-REF-001",
+            price=100
+        )
+        self.assertTrue(variant.barcode.startswith("BAR-"))
+        
