b868217a505281e99b22847b52812ee334ec9607
ali-614 <alhamdani840@gmail.com>
2026-07-09 15:39:27 +0300

    added views and url for product transfer

---
+++ inventory/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Variant, Location
+
+
+class TransferForm(forms.Form):
+    variant = forms.ModelChoiceField(queryset=Variant.objects.all())
+    source = forms.ModelChoiceField(queryset=Location.objects.all())
+    destination = forms.ModelChoiceField(queryset=Location.objects.all())
+    quantity = forms.IntegerField(min_value=1)
+
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from .models import Transfer
+from .forms import TransferForm
 
-# Create your views here.
+def transfer_stock(request):
+    if request.method == "POST":
+        form = TransferForm(request.POST)
+        if form.is_valid():
+            transfer = Transfer.objects.create(
+                variant = form.cleaned_data["variant"],
+                source = form.cleaned_data["source"],
+                destination = form.cleaned_data["destination"],
+                quantity = form.cleaned_data["quantity"],)
+            
+            try:
+                transfer.execute()
+                messages.success(request, "Transfer completed successfully")
+                return redirect("transfer_stock")
+            except ValidationError as e:
+                transfer.delete()
+                messages.error(request, e.message)
+
+
+    else:
+        form = TransferForm()
+
+
+    return render(request, "inventory/transfer.html", {"form":form})
